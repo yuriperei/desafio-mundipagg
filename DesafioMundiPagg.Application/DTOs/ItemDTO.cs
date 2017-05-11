2dbcb177fbe174a45396725658327d10dd640e85
yuriperei <yuri.souza@al.infnet.edu,br>
2017-05-10 23:34:54 -0300

    Insere ItemDTO

---
+++ DesafioMundiPagg.Application/DTOs/ItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioMundiPagg.Application.DTOs
+{
+    public class ItemDTO
+    {
+        public string ItemId { get; set; }
+        public string Titulo { get; set; }
+        public TipoItem TipoItem { get; set; }
+        public bool IsEmprestado { get; set; }
+    }
+}
