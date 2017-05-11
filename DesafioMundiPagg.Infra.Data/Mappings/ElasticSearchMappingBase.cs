b2ab1e97b959a106de58de989c0932e184d7aef3
yuriperei <yuri.souza@al.infnet.edu,br>
2017-05-11 16:57:48 -0300

    Remove MapEntityValues

--- DesafioMundiPagg.Infra.Data/Mappings/ElasticSearchMappingBase.cs
+++ DesafioMundiPagg.Infra.Data/Mappings/ElasticSearchMappingBase.cs
@@ -9,17 +9,6 @@ namespace DesafioMundiPagg.Infra.Data.Mapping
 {
     public abstract class ElasticSearchMappingBase : ElasticsearchMapping
     {
-
-        public override void MapEntityValues(EntityContextInfo entityInfo, ElasticsearchCrudJsonWriter elasticsearchCrudJsonWriter, bool beginMappingTree = false, bool createPropertyMappings = false)
-        {
-            var propertyInfo = entityInfo.Document.GetType().GetProperties();
-            foreach (var prop in propertyInfo)
-            {
-                MapValue(prop.Name, prop.GetValue(entityInfo.Document), elasticsearchCrudJsonWriter.JsonWriter);
-            }
-
-        }
-
         public override string GetDocumentType(Type type)
         {
             return type.Name.ToLower();
