e01d21146a9100ac2bb2d3e5f66f0e165cfa8efb
yuriperei <yuri.souza@al.infnet.edu,br>
2017-05-14 02:11:43 -0300

    Corrige mapeamento de rotas

--- DesafioMundiPagg.Presentation/ClientApp/app/app.routes.ts
+++ DesafioMundiPagg.Presentation/ClientApp/app/app.routes.ts
@@ -2,15 +2,22 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ItemListagemComponent } from './components/item/item-listagem.component';
 import { ItemFormularioComponent } from './components/item/item-formulario.component';
+import { PessoaFormularioComponent } from './components/pessoa/pessoa-formulario.component';
+import { PessoaListagemComponent } from './components/pessoa/pessoa-listagem.component';
 
 const appRoutes: Routes = [
 
     { path: '', redirectTo: 'itens', pathMatch: 'full' },
+
     { path: 'itens', component: ItemListagemComponent },
     { path: 'cadastrar/item', component: ItemFormularioComponent },
     { path: 'alterar/item/:id', component: ItemFormularioComponent },
-    { path: '**', redirectTo: 'itens' }
 
+    { path: 'pessoas', component: PessoaListagemComponent },
+    { path: 'cadastrar/pessoa', component: PessoaFormularioComponent },
+    { path: 'alterar/pessoa/:id', component: PessoaFormularioComponent },
+    
+    { path: '**', redirectTo: 'itens' }
 ];
 
 @NgModule({
