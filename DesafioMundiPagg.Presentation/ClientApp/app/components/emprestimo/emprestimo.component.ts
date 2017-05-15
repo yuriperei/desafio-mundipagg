328fe947ff2952eb89f8ad3aabe4feefc472982d
yuriperei <yuri.souza@al.infnet.edu,br>
2017-05-15 02:25:01 -0300

    Insere estrutura para entidade Emprestimo

---
+++ DesafioMundiPagg.Presentation/ClientApp/app/components/emprestimo/emprestimo.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component, Input } from '@angular/core';
+import { ItemComponent } from '../item/item.component';
+
+
+@Component({
+    selector: 'emprestimo',
+})
+export class EmprestimoComponent {
+    emprestimoId: string;
+    @Input() dataDevolucao: string;
+    @Input() dataEmprestimo: string;
+    @Input() item: ItemComponent;
+}
---
+++ DesafioMundiPagg.Presentation/ClientApp/app/modules/emprestimo.module.ts
@@ -0,0 +1,31 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DataTableModule } from "angular2-datatable";
+import { HttpModule } from "@angular/http";
+
+import { EmprestimoFiltroPipe } from '../pipes/emprestimo-filtro.pipe';
+
+import { EmprestimoService } from '../services/emprestimo.service';
+
+import { EmprestimoListagemComponent } from '../components/emprestimo/emprestimo-listagem.component';
+import { EmprestimoFormularioComponent } from '../components/emprestimo/emprestimo-formulario.component';
+
+import { MyDatePickerModule } from 'mydatepicker';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule, ReactiveFormsModule,
+        DataTableModule, 
+        HttpModule,
+        RouterModule,
+        MyDatePickerModule 
+    ],
+    declarations: [EmprestimoListagemComponent, EmprestimoFormularioComponent, EmprestimoFiltroPipe],
+    exports: [EmprestimoListagemComponent, EmprestimoFormularioComponent ],
+    providers: [EmprestimoService]
+})
+
+export class EmprestimoModule { }
